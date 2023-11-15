6aee98b8889365bfe8fc9ea3a0c61d9a30aa07e9
jeyasrivenkat <34196788+jeyasrivenkat@users.noreply.github.com>
2023-11-14 17:18:22 -0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import {View} from 'react-native'
+import GetStarted from './screens/GetStarted'
+
+export default class app extends Component{
+  render(){
+    return(
+      <View>
+         <GetStarted/>
+
+       
+      </View>
+    )
+  }
+
+  
+}
